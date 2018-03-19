bdb8f575eb0b9ed03cb42706fae1d4b420ce278d
Ulrik <dulrik08@gmail.com>
2018-03-18 21:56:10 -0300

    ScrollView added on details screen.

--- app/src/main/java/com/arctouch/codechallenge/ui/HomeActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/ui/HomeActivity.java
@@ -37,6 +37,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
         ButterKnife.bind(this);
+
         configRecycler();
         loadUpcomingMoviews(1);
     }
--- app/src/main/java/com/arctouch/codechallenge/viewmodel/UpcomingMoviesViewModel.java
+++ app/src/main/java/com/arctouch/codechallenge/viewmodel/UpcomingMoviesViewModel.java
@@ -4,13 +4,17 @@ import com.arctouch.codechallenge.App;
 import com.arctouch.codechallenge.api.TmdbApi;
 import com.arctouch.codechallenge.data.Cache;
 import com.arctouch.codechallenge.model.Genre;
+import com.arctouch.codechallenge.model.GenreResponse;
 import com.arctouch.codechallenge.model.Movie;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -19,7 +23,22 @@ import io.reactivex.schedulers.Schedulers;
 
 public class UpcomingMoviesViewModel implements ViewModel {
 
+    public Observable<GenreResponse> cache() {
+        return App.getApi().genres(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE)
+                .subscribeOn(Schedulers.io());
+    }
+
     public Observable<List<Movie>> getUpcomingMovies(long page) {
+        if (Cache.getGenres().isEmpty()) {
+            return cache()
+                    .flatMap(genreResponse -> {Cache.setGenres(genreResponse.genres); return Observable.empty();})
+                    .flatMap(o -> getListObservable(page));
+        } else {
+            return getListObservable(page);
+        }
+    }
+
+    private Observable<List<Movie>> getListObservable(long page) {
         return App.getApi().upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, page, TmdbApi.DEFAULT_REGION)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
